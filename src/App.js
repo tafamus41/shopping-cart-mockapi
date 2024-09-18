827e31d3eff688f058bc703eab9397d6e18307e3
asimov <seyranmustafa4@gmail.com>
2024-09-18 19:07:20 +0300

    p

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-
 import Main from "./pages/Main";
 import NewProduct from "./pages/NewProduct";
 import ProductList from "./pages/ProductList";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="bg-light app">
-      <Navbar/>
-      <Main/>
-      <About/>
-      <NewProduct/>
-      <ProductList/>
+      <Navbar />
+      <Routes>
+        <Routes exact path="/" element={<Main />} />
+      </Routes>
+
+      <About />
+      <NewProduct />
+      <ProductList />
     </div>
   );
 }
